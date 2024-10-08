d126959e475e633bb46d9f7a6191245b162ac24a
LEBaz2211 <92997786+LEBaz2211@users.noreply.github.com>
2024-10-08 11:17:17 +0200

    updated with dos2unix

--- clean_wsl.sh
+++ clean_wsl.sh
@@ -1,9 +1,13 @@
 cd data-nuxt-app
+dos2unix clean_wsl.sh
 ./clean_wsl.sh
 cd ../nginx-home
+dos2unix clean_wsl.sh
 ./clean_wsl.sh
 cd ../camera-app
+dos2unix clean_wsl.sh
 ./clean_wsl.sh
 cd ../map-app
+dos2unix clean_wsl.sh
 ./clean_wsl.sh
 cd ..
--- deploy_wsl.sh
+++ deploy_wsl.sh
@@ -1,9 +1,13 @@
 cd data-nuxt-app
+dos2unix deploy_wsl.sh
 ./deploy_wsl.sh
 cd ../nginx-home
+dos2unix deploy_wsl.sh
 ./deploy_wsl.sh
 cd ../camera-app
+dos2unix deploy_wsl.sh
 ./deploy_wsl.sh
 cd ../map-app
+dos2unix deploy_wsl.sh
 ./deploy_wsl.sh
 cd ..
