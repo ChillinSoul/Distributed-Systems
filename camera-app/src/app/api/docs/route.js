a557d0470bc6b9c550e0b045ed2cd30ae2779db5
Mettioui Mourad <mouradm.mm@outlook.com>
2024-12-03 14:27:40 +0100

    fixed swagger prob

--- camera-app/src/app/api/docs/route.js
+++
@@ -1,7 +0,0 @@
-import swaggerSpec from '../../../../swaggerconfig';
-
-export async function GET() {
-  return new Response(JSON.stringify(swaggerSpec), {
-    headers: { 'Content-Type': 'application/json' },
-  });
-}
